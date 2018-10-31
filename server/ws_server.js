09ad5f81d756ee7d56fb239eaf66a59001587881
Dominic Tremblay <dominictremblay24@gmail.com>
2018-10-31 02:59:07 -0400

    created the addClient function

--- server/ws_server.js
+++ server/ws_server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const SocketServer = require('ws');
+const uuidv4 = require('uuid/v4');
 
 const PORT = 3001;
 
@@ -10,3 +11,29 @@ const server = express()
   );
 
 const wss = new SocketServer.Server({ server });
+const clients = {};
+
+const generateColor = () => {
+  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+  let color = [0, 0, 0, 0, 0, 0, 0];
+
+  color = color.map(hexCode => hex[Math.floor(Math.random() * 16)]);
+  color[0] = '#';
+  return color.join('');
+};
+
+const addClient = (ws, username = 'Anonymous') => {
+  const clientId = uuidv4();
+  ws.clientId = clientId;
+  clients[clientId] = { ws, username, color: generateColor() };
+};
+
+wss.on('connection', ws => {
+  console.log('Client connected');
+  addClient(ws);
+  console.log(clients[ws.clientId]);
+});
+
+wss.on('close', () => {
+  console.log('Client disconnected');
+});
